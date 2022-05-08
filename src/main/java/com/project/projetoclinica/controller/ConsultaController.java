e8027ac0af3cb458059ec53049126ad1f87be75f
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-08 16:05:09 -0300

    consultaPost

--- src/main/java/com/project/projetoclinica/controller/ConsultaController.java
+++ src/main/java/com/project/projetoclinica/controller/ConsultaController.java
@@ -3,11 +3,9 @@ package com.project.projetoclinica.controller;
 import com.project.projetoclinica.domain.Consulta;
 import com.project.projetoclinica.service.ConsultaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +20,15 @@ public class ConsultaController {
     public ResponseEntity<List<Consulta>> list() {
         return ResponseEntity.ok(consultaService.list());
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Consulta> findById(@PathVariable long id) {
         return ResponseEntity.ok(consultaService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Consulta> save(@RequestBody Consulta consulta) {
+        return new ResponseEntity<>(consultaService.save(consulta), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/project/projetoclinica/service/ConsultaService.java
+++ src/main/java/com/project/projetoclinica/service/ConsultaService.java
@@ -6,32 +6,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
 public class ConsultaService {
 
-    MedicoService medicoService = new MedicoService();
-    PacienteService pacienteService = new PacienteService();
+    static MedicoService medicoService = new MedicoService();
+    static PacienteService pacienteService = new PacienteService();
 
-    private List<Consulta> consultas = List.of(new Consulta(
-            1L,
-            medicoService.findById(1L),
-            pacienteService.findById(1L),
-            "03/05/2022",
-            "16:30:00",
-            500,
-            "Dinheiro"
-    ), new Consulta(
-            2L,
-            medicoService.findById(2L),
-            pacienteService.findById(2L),
-            "04/05/2022",
-            "17:00:00",
-            1000,
-            "Dinheiro"
-    ));
+    private static List<Consulta> consultas;
+    static {
+        consultas = new ArrayList<>(List.of(new Consulta(
+                1L,
+                medicoService.findById(1),
+                pacienteService.findById(1),
+                "03/05/2022",
+                "16:30:00",
+                500,
+                "Dinheiro"
+        ), new Consulta(
+                2L,
+                medicoService.findById(2),
+                pacienteService.findById(2),
+                "04/05/2022",
+                "17:00:00",
+                1000,
+                "Dinheiro"
+        )));
+    }
 
     public List<Consulta> list() {
         return consultas;
@@ -40,6 +45,12 @@ public class ConsultaService {
     public Consulta findById(long id) {
         return consultas.stream().filter(consultas -> consultas.getId().equals(id)).findFirst().orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Consulta não encontrada."));
+    }
+
+    public Consulta save(Consulta consulta) {
+        consulta.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        consultas.add(consulta);
+        return consulta;
     }
 
 }
