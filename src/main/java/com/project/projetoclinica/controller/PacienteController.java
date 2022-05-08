da2db88da19e1efe8675d1d0a764065e5ef4d6a7
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-08 15:06:23 -0300

    pacientePost

--- src/main/java/com/project/projetoclinica/controller/PacienteController.java
+++ src/main/java/com/project/projetoclinica/controller/PacienteController.java
@@ -3,11 +3,9 @@ package com.project.projetoclinica.controller;
 import com.project.projetoclinica.domain.Paciente;
 import com.project.projetoclinica.service.PacienteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,11 @@ public class PacienteController {
     @GetMapping("/{id}")
     public ResponseEntity<Paciente> findById(@PathVariable long id) {
         return ResponseEntity.ok(pacienteService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Paciente> save(@RequestBody Paciente paciente) {
+        return new ResponseEntity<>(pacienteService.save(paciente), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/project/projetoclinica/service/PacienteService.java
+++ src/main/java/com/project/projetoclinica/service/PacienteService.java
@@ -7,36 +7,42 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class PacienteService {
 
-    private List<Paciente> pacientes = List.of(new Paciente(
-                    1L,
-                    "Pessoa1",
-                    "###.###.###-##",
-                    "emaildapessoa1@emails.com",
-                    "01/01/2001",
-                    "###########",
-                    "###########",
-                    "Programadora",
-                    "Rua das Ruas",
-                    "Familiar",
-                    new ArrayList<>(List.of("camarão", "amendoim")),
-                    new ArrayList<>(List.of("diabetes", "alzheimer", "osteoporose"))),
-                    new Paciente(
-                    2L,
-                    "Pessoa2",
-                    "###.###.###-##",
-                    "emaildapessoa2@emails.com",
-                    "01/01/2001",
-                    "###########",
-                    "###########",
-                    "Paleontóloga",
-                    "Rua das Sem Saída",
-                    "Familiar",
-                    new ArrayList<>(List.of("água", "ovos")),
-                    new ArrayList<>(List.of("câncer", "asma"))));
+    private static List<Paciente> pacientes;
+    static {
+        pacientes = new ArrayList<>(List.of(new Paciente(
+                        1L,
+                        "Pessoa1",
+                        "###.###.###-##",
+                        "emaildapessoa1@emails.com",
+                        "01/01/2001",
+                        "###########",
+                        "###########",
+                        "Programadora",
+                        "Rua das Ruas",
+                        "Familiar",
+                        new ArrayList<>(List.of("camarão", "amendoim")),
+                        new ArrayList<>(List.of("diabetes", "alzheimer", "osteoporose"))),
+                new Paciente(
+                        2L,
+                        "Pessoa2",
+                        "###.###.###-##",
+                        "emaildapessoa2@emails.com",
+                        "01/01/2001",
+                        "###########",
+                        "###########",
+                        "Paleontóloga",
+                        "Rua das Sem Saída",
+                        "Familiar",
+                        new ArrayList<>(List.of("água", "ovos")),
+                        new ArrayList<>(List.of("câncer", "asma")))));
+
+
+    }
 
     public List<Paciente> list() {
         return pacientes;
@@ -47,5 +53,9 @@ public class PacienteService {
                 () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Paciente não encontrado."));
     }
 
-
+    public Paciente save(Paciente paciente) {
+        paciente.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        pacientes.add(paciente);
+        return paciente;
+    }
 }
