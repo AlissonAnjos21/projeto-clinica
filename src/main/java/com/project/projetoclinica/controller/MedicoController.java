5b22f2c9a58f2f78724261467912b1867a6f93e0
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-08 14:07:48 -0300

    medicoPost

--- src/main/java/com/project/projetoclinica/controller/MedicoController.java
+++ src/main/java/com/project/projetoclinica/controller/MedicoController.java
@@ -3,11 +3,9 @@ package com.project.projetoclinica.controller;
 import com.project.projetoclinica.domain.Medico;
 import com.project.projetoclinica.service.MedicoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,11 @@ public class MedicoController {
     @GetMapping("/{id}")
     public ResponseEntity<Medico> findById(@PathVariable long id) {
         return ResponseEntity.ok(medicoService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Medico> save(@RequestBody Medico medico) {
+        return new ResponseEntity<>(medicoService.save(medico), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/project/projetoclinica/service/MedicoService.java
+++ src/main/java/com/project/projetoclinica/service/MedicoService.java
@@ -5,36 +5,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class MedicoService {
 
-    private List<Medico> medicos = List.of(new Medico(
-            1L,
-            "Raquel",
-            "###.###.###-##",
-            " XX.XXX.XXX/0001-XX",
-            "CRM/BA 123456",
-            "05/12/2003",
-            "Rua Muito Ruim",
-            "emaildaraquel@emails.com",
-            "###########",
-            "Público",
-            "Psiquiatria"),
-            new Medico(
-                    2L,
-                    "Caio",
-                    "###.###.###-##",
-                    " XX.XXX.XXX/0001-XX",
-                    "CRM/BA 456789",
-            "29/03/2004",
-                    "Rua Muito Boa",
-                    "emaildocaio@emails.com",
-            "###########",
-                    "Privado",
-                    "Infectologia"
-    ));
+    private static List<Medico> medicos;
+    static {
+        medicos = new ArrayList<>(List.of(new Medico(
+                        1L,
+                        "Raquel",
+                        "###.###.###-##",
+                        "XX.XXX.XXX/0001-XX",
+                        "CRM/BA 123456",
+                        "05/12/2003",
+                        "Rua Muito Ruim",
+                        "emaildaraquel@emails.com",
+                        "###########",
+                        "Público",
+                        "Psiquiatria"),
+                new Medico(
+                        2L,
+                        "Caio",
+                        "###.###.###-##",
+                        "XX.XXX.XXX/0001-XX",
+                        "CRM/BA 456789",
+                        "29/03/2004",
+                        "Rua Muito Boa",
+                        "emaildocaio@emails.com",
+                        "###########",
+                        "Privado",
+                        "Infectologia"
+                )));
+    }
 
     public List<Medico> list() {
         return medicos;
@@ -43,6 +48,12 @@ public class MedicoService {
     public Medico findById(long id) {
         return medicos.stream().filter(medico -> medico.getId().equals(id)).findFirst().orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.BAD_REQUEST, "Médico não encontrado."));
+    }
+
+    public Medico save(Medico medico) {
+        medico.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        medicos.add(medico);
+        return medico;
     }
 
 }
