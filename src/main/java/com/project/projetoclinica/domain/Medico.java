0ddc86d6c265356898b6f8b9bb3377ef4f33d902
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-07-29 08:06:20 -0300

    Conexao BD

--- src/main/java/com/project/projetoclinica/domain/Medico.java
+++ src/main/java/com/project/projetoclinica/domain/Medico.java
@@ -2,10 +2,20 @@ package com.project.projetoclinica.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String cpf;
--- src/main/java/com/project/projetoclinica/domain/Paciente.java
+++ src/main/java/com/project/projetoclinica/domain/Paciente.java
@@ -2,12 +2,21 @@ package com.project.projetoclinica.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String cpf;
