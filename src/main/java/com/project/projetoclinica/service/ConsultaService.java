07038a7d4c84ac65b0ee2add04c5655d6c5db1be
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-08-01 14:14:49 -0300

    Melhorias

--- src/main/java/com/project/projetoclinica/service/ConsultaService.java
+++ src/main/java/com/project/projetoclinica/service/ConsultaService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
--- src/main/java/com/project/projetoclinica/service/PacienteService.java
+++ src/main/java/com/project/projetoclinica/service/PacienteService.java
@@ -10,9 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
